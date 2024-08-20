0c11f60ee4e52d08249e96bf02cbf15bbb2b5eaf
MrHell228 <artemkz2016@gmail.com>
2024-08-21 03:38:29 +0700

    Create spongeneoforge.settings.gradle.kts

---
+++ spongeneoforge.settings.gradle.kts
@@ -0,0 +1,4 @@
+// Uncomment to enable SpongeForge module.
+// By default only Sponge and SpongeVanilla are made available
+//include(":SpongeNeoForge")
+//project(":SpongeNeoForge").projectDir = file("neoforge")
